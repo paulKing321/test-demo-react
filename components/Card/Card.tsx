a36d782bf0722fd54de69c5e2d9604e93b97a223
Paul Adeoye <oluwadamilare321@gmail.com>
2023-01-26 01:27:52 +0100

    card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,5 +1,22 @@
-const Card = () => <div>
-    Card
-</div>
+// Components
+import Thumb from '../Thumb/Thumb';
+
+type Props = {
+  imgUrl: string;
+  title: string;
+  subtitle?: string;
+};
+
+const Card = ({ imgUrl, title, subtitle }: Props) => (
+  <div className='h-80'>
+    <div className='relative h-full'>
+      <Thumb imgUrl={imgUrl} />
+      <div className='absolute bottom-0 w-full px-4 py-2 rounded-b-xl bg-zinc-800'>
+        <h2 className='text-sm text-center truncate text-cyan-200'>{title}</h2>
+        {subtitle ? <p className='text-xs text-center truncate text-cyan-400'>{subtitle}</p> : null}
+      </div>
+    </div>
+  </div>
+);
 
 export default Card;
