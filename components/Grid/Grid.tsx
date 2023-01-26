c9363c662701b313a17901c6b266002a47862ed2
Paul Adeoye <oluwadamilare321@gmail.com>
2023-01-26 01:28:13 +0100

    grid component

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -1,5 +1,14 @@
-const Grid = () => <div>
-    Grid
-</div>
-
-export default Grid;
+type Props = {
+    title: string;
+    children: React.ReactNode;
+    className?: string;
+  };
+  
+  const Grid = ({ title, children, className }: Props) => (
+    <div className={className}>
+      <h2 className='pb-4 text-xl font-bold'>{title}</h2>
+      <div className='grid gap-8 grid-cols-auto-fill'>{children}</div>
+    </div>
+  );
+  
+  export default Grid;
